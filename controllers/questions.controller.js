4a6a42b196cfe3b791dce662d71e5db4578264cd
IrshadYasseenKaar <airshadyasseen@kaartech.com>
2025-03-20 18:20:59 +0530

    questions crud

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -5,11 +5,12 @@ const qHome = async (req, res) => {
     res.set(200).json("Questions home active")
 }
 
+//  All Questions
 const getAllQuestions = async (req, res) => {
     try {
         questions = await Questions.find()
         if (!questions.length) {
-            return(res.status(404).json("Not found"))
+            return(res.status(404).json("Questions not found"))
         }
         res.set(200).json(questions)
     }
@@ -18,7 +19,43 @@ const getAllQuestions = async (req, res) => {
     }
 }
 
+const postQuestions = async (req, res) => {
+    try {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return res.status(400).json({ error: "Request body cannot be empty" });
+        }
+
+        const question = await Questions.create(req.body); // Fix: Variable name
+
+        res.status(201).json(question); // Fix: Status 201 for resource creation
+    } 
+    catch (e) {
+        res.status(500).json({ error: e.message }); // Fix: Better error handling
+    }
+};
+
+const deleteOne= async (req, res) => {
+    try {
+        console.log("delete trigged")
+
+        const { q_no } = req.params;
+        const question = await Questions.findOneAndDelete({q_no: q_no});
+        if (!question) {
+            return res.status(404).json({ 'message': 'Question not found' });
+        }
+        res.status(200).json({"message":"deleted ",'Question':question});
+        
+    }
+    catch (err) {
+        res.status(500).json({ 'message': err.message });
+
+    }
+}
+
+
 module.exports = {
     qHome,
-    getAllQuestions
+    getAllQuestions,
+    postQuestions,
+    deleteOne
 }
--- router/questions.route.js
+++ router/questions.route.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const {qHome,getAllQuestions} = require("../controllers/questions.controller")
+const {qHome,getAllQuestions,postQuestions,deleteOne} = require("../controllers/questions.controller")
 const router = express.Router();
 
 router.get('/',qHome)
-// router.get("/all", getAllQuestions)
+router.get("/all", getAllQuestions)
 // router.get("/:id", getOneAnswerById)
 // // router.get("/:st_id/:an_no", getOneAnswerByKey)
 
-// router.post("/all", postAnswer)
+ router.post("/all", postQuestions)
 
 // router.put("/:st_id/:an_no", putOneByKey)
 
-// router.delete("/:st_id/:an_no", deleteOneByKey)
+router.delete("/:q_no", deleteOne)
 
 module.exports= router
